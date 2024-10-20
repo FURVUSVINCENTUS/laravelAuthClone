3745453fa605d74666aec82ba1fba67a25533c0b
Vicente Crestani <vicente.crestani@edu.vs.ch>
2024-10-20 21:00:54 +0200

    build-script.sh mv -> cp for .env.base

--- build-script.sh
+++ build-script.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 cd ./authentication-be && composer install
-mv ./.env.base ./.env
+cp ./.env.base ./.env
+php artisan key:generate
 php artisan migrate
 php artisan vendor:publish --provider="PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider"
 php artisan jwt:secret | awk '{print "JWT_SECRET="$0}' >> output_file
